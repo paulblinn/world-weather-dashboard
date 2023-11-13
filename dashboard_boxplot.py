13a1d5ec7da2fcbc1271e49b03bed74b6aa63b17
Paul Blinn <102709407+paulblinn@users.noreply.github.com>
2023-11-12 18:19:40 -0800

    Update dashboard_boxplot.py

--- dashboard_boxplot.py
+++ dashboard_boxplot.py
@@ -5,6 +5,7 @@
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 
+# Set up the figure and adjust plot parameters
 plt.figure(figsize=(4.6, 2.3))
 plt.subplots_adjust(left=0.03, right=0.97, top=0.88, bottom=0.12)
 plt.gca().spines['top'].set_color('#FFFFFF')
@@ -12,15 +13,31 @@ plt.gca().spines['bottom'].set_color('#FFFFFF')
 plt.gca().spines['left'].set_color('#FFFFFF')
 plt.gca().spines['right'].set_color('#FFFFFF')
 
-plt.boxplot(dataset, vert=False, widths = 0.5, boxprops={'linewidth': 1.6,'color': '#FFFFFF'}, whiskerprops={'linewidth': 1.6,'color': '#FFFFFF'}, capprops={'linewidth': 1.6,'color': '#FFFFFF'}, medianprops={'linewidth': 2,'color': '#F2CE16'})
+# Create the box plot
+plt.boxplot(dataset, vert=False, widths=0.5,
+            boxprops={'linewidth': 1.6, 'color': '#FFFFFF'},
+            whiskerprops={'linewidth': 1.6, 'color': '#FFFFFF'},
+            capprops={'linewidth': 1.6, 'color': '#FFFFFF'},
+            medianprops={'linewidth': 2, 'color': '#F2CE16'})
+
+# Customize grid appearance
 plt.gca().grid(True, linestyle=':', linewidth=1.5, alpha=0.7, color='#FFFFFF')
+
+# Set title and customize font properties
 plt.title('Visibility Range (mi)', fontdict={'family': 'Consolas', 'size': 18}, color='#FFFFFF')
+
+# Customize x-axis ticks
 plt.xticks(fontproperties=FontProperties(family='Consolas', size=15), color='#FFFFFF')
 plt.gca().tick_params(axis='x', colors='#FFFFFF')
+
+# Hide y-axis ticks
 plt.yticks([])
 
+# Set background colors
 plt.gcf().set_facecolor('#386273')
 plt.gca().set_facecolor('#386273')
 
+# Display the plot
 plt.grid(True)
 plt.show()
+
